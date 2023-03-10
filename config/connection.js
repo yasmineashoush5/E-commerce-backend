91ca98945bdc0368d46751a3806e3c445db2397b
yasmine <yasmineashoush5@gmail.com>
2023-03-10 00:38:30 -0500

    seeds

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,12 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+});
 
-
-  // const sequelize = process.env.JAWSDB_URL
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  });
-}
 module.exports = sequelize;
